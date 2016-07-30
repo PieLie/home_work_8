36a4b9782dd8b2f7c5c2acc36aa1c7f99c2e4786
slava vakulenko <pro100slavik12071993@gmail.com>
2016-07-31 01:14:04 +0400

    dedline two

--- js/common.js
+++ js/common.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	$('#mobile-mnu-btn').on('click', function () {
 
-		if ($('.mobile-mnu__item').hasClass('show')) {
-			$('.mobile-mnu__item').removeClass('show');
-			$('.mobile-mnu__list').removeClass('show-list')
+		if ($('.main-nav__item').hasClass('show')) {
+			$('.main-nav__item').removeClass('show');
+			$('.main-nav__list').removeClass('show-list')
 
 		}
 		else {
-			$('.mobile-mnu__item').addClass('show');
-			$('.mobile-mnu__list').addClass('show-list')
+			$('.main-nav__item').addClass('show');
+			$('.main-nav__list').addClass('show-list')
 		}
 				
 	})
